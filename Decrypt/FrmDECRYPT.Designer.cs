8f024803a4aad97570e7cd65a52248579dea2f6a
msmonroe <msmonroe@twelvetwentyone.com>
2015-06-30 17:51:11 -0700

    Simple Decryption Program

---
+++ Decrypt/FrmDECRYPT.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Decrypt
+{
+    partial class FrmDECRYPT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDECRYPT = new System.Windows.Forms.Button();
+            this.lblDECRYPT = new System.Windows.Forms.Label();
+            this.txtBxDECRYPT = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDECRYPT
+            // 
+            this.btnDECRYPT.Location = new System.Drawing.Point(13, 57);
+            this.btnDECRYPT.Name = "btnDECRYPT";
+            this.btnDECRYPT.Size = new System.Drawing.Size(259, 23);
+            this.btnDECRYPT.TabIndex = 0;
+            this.btnDECRYPT.Text = "DECRYPT";
+            this.btnDECRYPT.UseVisualStyleBackColor = true;
+            this.btnDECRYPT.Click += new System.EventHandler(this.btnDECRYPT_Click);
+            // 
+            // lblDECRYPT
+            // 
+            this.lblDECRYPT.AutoSize = true;
+            this.lblDECRYPT.Location = new System.Drawing.Point(16, 8);
+            this.lblDECRYPT.Name = "lblDECRYPT";
+            this.lblDECRYPT.Size = new System.Drawing.Size(0, 13);
+            this.lblDECRYPT.TabIndex = 1;
+            // 
+            // txtBxDECRYPT
+            // 
+            this.txtBxDECRYPT.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxDECRYPT.Location = new System.Drawing.Point(13, 27);
+            this.txtBxDECRYPT.Name = "txtBxDECRYPT";
+            this.txtBxDECRYPT.Size = new System.Drawing.Size(259, 24);
+            this.txtBxDECRYPT.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 88);
+            this.Controls.Add(this.txtBxDECRYPT);
+            this.Controls.Add(this.lblDECRYPT);
+            this.Controls.Add(this.btnDECRYPT);
+            this.Name = "Form1";
+            this.Text = "Decrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDECRYPT;
+        private System.Windows.Forms.Label lblDECRYPT;
+        private System.Windows.Forms.TextBox txtBxDECRYPT;
+    }
+}
+
---
+++ Decrypt/FrmDECRYPT.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decrypt
+{
+    public partial class FrmDECRYPT : Form
+    {
+        private const string ENDEROT = @"0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_abcdefghijklmnopqrstuvwxyz";
+        private const string ERROR = "Error";
+
+        public FrmDECRYPT()
+        {
+
+            InitializeComponent();
+
+            PrivateFontCollection pfc = new PrivateFontCollection();
+
+            pfc.AddFontFile(@"Roboto\Roboto-Regular.ttf");
+            
+            lblDECRYPT.Font = new Font(pfc.Families[0], 10, FontStyle.Regular);
+            
+            txtBxDECRYPT.Font = new Font(pfc.Families[0], 10, FontStyle.Regular);
+
+            lblDECRYPT.Text = "";
+        }
+
+        public string decrypto(string d)
+        {
+            try
+            {
+                string o = "";
+
+                if (false == string.IsNullOrEmpty(d))
+                {
+                    string s = d.Trim();
+                    int l = s.Length;
+
+                    for (int i = 0; i < l; i++)
+                    {
+                        int x = ENDEROT.IndexOf(s.Substring(i, 1)) - i;
+
+                        if (x < 0)
+                        {
+                            x = i;
+                        }
+
+                        o += ENDEROT.Substring(x, 1);
+                    }
+                }
+
+                return o;
+            }
+            catch
+            {
+                return ERROR;
+            }
+        }
+
+        private void btnDECRYPT_Click(object sender, EventArgs e)
+        {
+            lblDECRYPT.Text = decrypto(txtBxDECRYPT.Text);
+        }
+    }
+}
