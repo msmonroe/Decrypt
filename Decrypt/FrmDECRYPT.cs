bd39ef1243e7bed729c90541cb2b4e962c8d36a3
msmonroe <msmonroe@twelvetwentyone.com>
2015-06-30 18:04:18 -0700

    Unknown Change

--- Decrypt/FrmDECRYPT.cs
+++ Decrypt/FrmDECRYPT.cs
@@ -29,7 +29,7 @@ namespace Decrypt
             
             txtBxDECRYPT.Font = new Font(pfc.Families[0], 10, FontStyle.Regular);
 
-            lblDECRYPT.Text = "";
+            lblDECRYPT.Text = "                    ";
         }
 
         public string decrypto(string d)
